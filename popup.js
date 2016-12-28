7e3f0ad172f8bbdb64368b1ded1130a1daf298d0
ConradCh <Conrad.chann@gmail.com>
2016-12-28 18:28:03 -0500

    Quality of life changes.

--- popup.js
+++ popup.js
@@ -6,6 +6,23 @@ function handleClick() {
 }
 
 $(document).ready(function () {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+    var h = today.getHours();
+    if(dd<10){
+        dd='0'+dd
+    }
+    if(mm<10){
+        mm='0'+mm
+    }
+
+    today = yyyy+'-'+mm+'-'+dd;
+    h = h + 1;
+    document.getElementById("dateField").setAttribute("min", today);
+    document.getElementById("dateField").valueAsDate = new Date();
+    document.getElementById("time").selectedIndex = h;
+
     $("#button").click(handleClick);
 });
-
